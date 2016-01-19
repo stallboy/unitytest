1ba62fb5889cb0a931432fc67f49ebba68229494
stallboy <stallboy@qq.com>
2016-01-20 00:49:08 +0800

    dolly zone

---
+++ Assets/DollyZoom.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class DollyZoom : MonoBehaviour
+{
+    public Transform target;
+    public Camera myCamera;
+    
+    private float initViewPointY;
+    public bool dzEnabled;
+    
+    void StartDZ()
+    {
+        initViewPointY = myCamera.WorldToViewportPoint(target.position).y;
+        dzEnabled = true;
+    }
+
+    void Start()
+    {
+        StartDZ();
+    }
+
+    void Update()
+    {
+        if (dzEnabled)
+        {
+            var plane = new Plane(myCamera.transform.up, myCamera.transform.position);
+            float hh = plane.GetDistanceToPoint(target.position) / initViewPointY;
+            var clipplane = new Plane(-myCamera.transform.forward, target.position);
+            float dist = clipplane.GetDistanceToPoint(myCamera.transform.position);
+            myCamera.fieldOfView = Mathf.Abs( 2.0f * Mathf.Atan(hh / dist) * Mathf.Rad2Deg );
+        }
+        transform.Translate(Input.GetAxis("Vertical") * Vector3.forward * Time.deltaTime * 5f);
+    }
+
+}
