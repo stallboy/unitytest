2392a323c8262c7f2444cff1c831817100e0b3c8
cheng xiaosan <stallboy@qq.com>
2016-11-23 19:44:23 +0800

    test assetbundle build rule

---
+++ Assets/Editor/ABChecker.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace BuildSystem
+{
+    public class ABChecker
+    {
+        private class DepInfo
+        {
+            public HashSet<string> containingABs = new HashSet<string>();
+
+            public int memsize;
+        }
+
+        private static Dictionary<string, DepInfo> depasset2bundles;
+        private static Dictionary<string, string> asset2bundle;
+
+        private static void collectDep(string asset, string bundle, int depth)
+        {
+            /*
+            if (depth == 0)
+            {
+                Debug.Log(asset + " --- " + bundle);
+            }
+            else if (depth < 10)
+            {
+                string res = "";
+                for (int i = 0; i < depth; i++)
+                {
+                    res += "    ";
+                }
+                Debug.Log(res + asset);
+            }
+            */
+
+            foreach (var depasset in AssetDatabase.GetDependencies(asset, false))
+            {
+                if (!depasset.EndsWith(".cs") && !asset2bundle.ContainsKey(depasset))
+                {
+                    DepInfo res;
+                    if (depasset2bundles.TryGetValue(depasset, out res))
+                    {
+                        res.containingABs.Add(bundle);
+                    }
+                    else
+                    {
+                        res = new DepInfo();
+                        res.containingABs.Add(bundle);
+                        depasset2bundles.Add(depasset, res);
+                    }
+                    collectDep(depasset, bundle, depth + 1);
+                }
+            }
+        }
+
+        [MenuItem("Tool/Check AssetBundle Duplicate")]
+        public static void CheckDuplicate()
+        {
+            asset2bundle = new Dictionary<string, string>();
+            foreach (var bundle in AssetDatabase.GetAllAssetBundleNames())
+            {
+                foreach (var asset in AssetDatabase.GetAssetPathsFromAssetBundle(bundle))
+                {
+                    asset2bundle.Add(asset, bundle);
+                }
+            }
+
+            Debug.Log("asset count=" + asset2bundle.Count);
+            depasset2bundles = new Dictionary<string, DepInfo>();
+            foreach (var kv in asset2bundle)
+            {
+                collectDep(kv.Key, kv.Value, 0);
+            }
+
+            int canSaveMemSize = 0;
+            foreach (var kv in depasset2bundles)
+            {
+                var depinfo = kv.Value;
+                if (depinfo.containingABs.Count > 1)
+                {
+                    depinfo.memsize = calcSize(kv.Key);
+
+                    Debug.Log(kv.Key + " count=" + depinfo.containingABs.Count + ", memsize=" +
+                              depinfo.memsize);
+                    foreach (var containingAB in depinfo.containingABs)
+                    {
+                        Debug.Log("    " + containingAB);
+                    }
+                    canSaveMemSize += depinfo.memsize*(depinfo.containingABs.Count - 1);
+                }
+            }
+
+            Debug.Log("can save mem size=" + canSaveMemSize);
+        }
+
+        private static int calcSize(string asset)
+        {
+            var objs = AssetDatabase.LoadAllAssetsAtPath(asset);
+            var allsize = 0;
+            foreach (var obj in objs)
+            {
+                var size = Profiler.GetRuntimeMemorySize(obj);
+                allsize += size;
+            }
+            return allsize;
+        }
+    }
+}
--- Assets/Editor/Test.cs
+++ Assets/Editor/Test.cs
@@ -1,11 +1,57 @@
-﻿using UnityEditor;
+﻿using System.Text;
+using UnityEditor;
+using UnityEngine;
 
-public class Test  {
-
+public class Test
+{
     [MenuItem("Tool/Build AssetBundle")]
-    static void BuildAssetBundle ()
+    public static void BuildAssetBundle()
     {
-        BuildPipeline.BuildAssetBundles("Assets/StreamingAssets", BuildAssetBundleOptions.DeterministicAssetBundle, BuildTarget.StandaloneWindows);
+        BuildPipeline.BuildAssetBundles("Assets/StreamingAssets",
+            BuildAssetBundleOptions.DeterministicAssetBundle | BuildAssetBundleOptions.UncompressedAssetBundle,
+            BuildTarget.StandaloneWindows);
     }
-	
+
+    [MenuItem("Tool/PrintDep")]
+    public static void TestPrintDep()
+    {
+        Debug.Log("texture size=" +
+                  Profiler.GetRuntimeMemorySize(
+                      AssetDatabase.LoadAssetAtPath<Object>("Assets/testassetbundlebuildrule/texture.psd")));
+        Debug.Log("mat size=" +
+                  Profiler.GetRuntimeMemorySize(
+                      AssetDatabase.LoadAssetAtPath<Object>("Assets/testassetbundlebuildrule/mat.mat")));
+
+        printDep("Assets/testassetbundlebuildrule/mat.mat", 0);
+
+        printDep("Assets/testassetbundlebuildrule/prefab2.prefab", 0);
+    }
+
+    private static void printDep(string asset, int depth)
+    {
+        //var obj = AssetDatabase.LoadAssetAtPath<Object>(asset);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < depth; i++)
+        {
+            sb.Append("    ");
+        }
+        sb.Append(asset);
+        sb.Append(", ");
+        var objs = AssetDatabase.LoadAllAssetsAtPath(asset);
+        var allsize = 0;
+        foreach (var obj in objs)
+        {
+            var size = Profiler.GetRuntimeMemorySize(obj);
+            allsize += size;
+            sb.Append("obj=" + obj + ",size=" + size + "\n");
+        }
+        
+        
+        Debug.Log(sb);
+        
+        foreach (var depasset in AssetDatabase.GetDependencies(asset, false))
+        {
+            printDep(depasset, depth + 1);
+        }
+    }
 }
